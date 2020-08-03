097877c794899156f1cb38b1a256a38636a359d3
Prabhat <prabhat.tanwar@twinline.in>
2020-08-03 15:52:57 +0530

    Added Recipe Service . Closes #7

---
+++ src/main/java/com/example/recipe/service/RecipeService.java
@@ -0,0 +1,10 @@
+package com.example.recipe.service;
+
+import com.example.recipe.domain.Recipe;
+
+import java.util.Set;
+
+public interface RecipeService  {
+
+    Set<Recipe> getAllRecipes();
+}
---
+++ src/main/java/com/example/recipe/service/RecipeServiceImpl.java
@@ -0,0 +1,27 @@
+package com.example.recipe.service;
+
+
+import com.example.recipe.domain.Recipe;
+import com.example.recipe.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class RecipeServiceImpl implements RecipeService {
+
+    private final RecipeRepository recipeRepository;
+
+    public RecipeServiceImpl(RecipeRepository recipeRepository) {
+        this.recipeRepository = recipeRepository;
+    }
+
+    @Override
+    public Set<Recipe> getAllRecipes() {
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add );
+        return recipeSet;
+    }
+
+}
