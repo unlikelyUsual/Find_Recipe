27ce1a4b45701af89bfe2760c3e3668ea192d304
Prabhat <prabhat.tanwar@twinline.in>
2020-08-04 20:42:17 +0530

    Added Integration Testing And Added FailSafe Plugin . Closes #11

--- src/test/java/com/example/recipe/controller/IndexControllerTest.java
+++ src/test/java/com/example/recipe/controller/IndexControllerTest.java
@@ -1,11 +1,16 @@
 package com.example.recipe.controller;
 
+import com.example.recipe.domain.Recipe;
 import com.example.recipe.service.RecipeService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
+
+import java.util.HashSet;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
@@ -32,8 +37,19 @@ class IndexControllerTest {
         String index = "index";
         assertEquals(index,indexController.getIndex(model));
 
+        Recipe recipe = new Recipe();
+        Recipe recipe1 = new Recipe();
+        recipe1.setId(33L);
+        Set<Recipe> recipeSet = new HashSet<>();
+        recipeSet.add(recipe);
+        recipeSet.add(recipe1);
+        when(recipeService.getAllRecipes()).thenReturn(recipeSet);
+
+        ArgumentCaptor<Set<Recipe>> recipeParameter = ArgumentCaptor.forClass(Set.class);
+
         verify(recipeService,times(1)).getAllRecipes();
-        verify(model,times(1)).addAttribute(eq("recipes"),anySet());
+        verify(model,times(1)).addAttribute(eq("recipes"),recipeParameter.capture());
+        //assertEquals(2,recipeParameter.getValue().size());
 
 
     }
