76cd734dbd412f92de21afe614877ec3d1ff542c
Prabhat <prabhat.tanwar@twinline.in>
2020-08-07 22:59:09 +0530

    Added Command Bean Object. Closes #14

---
+++ src/main/java/com/example/recipe/commands/RecipeCommand.java
@@ -0,0 +1,44 @@
+package com.example.recipe.commands;
+
+import com.example.recipe.domain.Category;
+import com.example.recipe.domain.Ingredient;
+import com.example.recipe.domain.Notes;
+import com.example.recipe.enums.Difficulty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+public class RecipeCommand {
+
+    private Long id;
+
+    private String description;
+
+    private Integer prepTime;
+
+    private Integer cookTime;
+
+    private Integer serving;
+
+    private String source;
+
+    private String url;
+
+    private String directions;
+
+    private byte[] image;
+
+    private Difficulty difficulty;
+
+    private Set<Ingredient> ingredients = new HashSet<>();
+
+    private Notes notes;
+
+    private Set<Category> categories = new HashSet<>();
+
+    private String imageString;
+}
